24184381fed4ea9db9e73abed4d8cbacfedacbd0
billysusanto <billysusanto@live.com>
2021-08-21 16:42:24 +0700

    first commit

---
+++ main.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("rare-journal-322614-3278c7cd5919.json")
+
+  project = "rare-journal-322614"
+  region  = "asia-southeast1"
+  zone    = "asia-southeast1-b"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+  tags = ["web", "dev"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+}
